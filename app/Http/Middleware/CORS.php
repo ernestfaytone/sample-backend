53aa5e0ba986a42f79b91e503541565471bd6497
ernestfaytonenavagis <ernest.faytone@navagis.com>
2020-12-26 00:02:56 +0800

    add header

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -7,6 +7,7 @@ class CORS {
     
     public function handle($request, Closure $next) {
         header('Acess-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         header('Acess-Control-Allow-Origin: Content-type, X-Auth-Token, Authorization, Origin');
         return $next($request);
     }
